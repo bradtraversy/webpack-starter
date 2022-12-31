7d5bf52c642cb1f241408d84e25d5e29448a4935
Robert Frank <trobfrank92@gmail.com>
2022-12-31 15:39:49 -0500

    Update webpack.config.js
    adding period between filename and content hash, as seen in the docs:
    https://webpack.js.org/configuration/output/#outputfilename

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name][contenthash].js',
+    filename: '[name].[contenthash].js',
     clean: true,
     assetModuleFilename: '[name][ext]',
   },
